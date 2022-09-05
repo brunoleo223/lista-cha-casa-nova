d01cb0595eb7b2dc407e4cecd32e2e51efc76d36
Bea <105248712+borgesanabea@users.noreply.github.com>
2022-09-04 22:12:43 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,7 +10,7 @@ export function Header() {
             </div>
             <div className={styles.header__perfil}>
                 <img src="/bea.jpg" alt="Vite logo" />
-                <h1>eu mudei 🫶🏻,</h1>
+                <h1>eu mudei <span> <img src="/coracao.jpg" style="width: 60px" alt="imagem de coração" /> </span>,</h1>
                 <h3> bora comemorar juntos essa nova etapa da minha vida?  </h3>
             </div>
         </header>
